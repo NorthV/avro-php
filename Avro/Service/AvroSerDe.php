8cee512c4dbf6f5d197261a0609e585809ebc213
Mikl <mikl@mediagraphics.ru>
2019-07-11 15:05:33 +0300

    Avro/Service/AvroSerDe.php imported

---
+++ Avro/Service/AvroSerDe.php
@@ -0,0 +1,63 @@
+<?php /* й */
+
+namespace Avro\Service;
+
+use Avro\DataIO\DataIO;
+use Avro\DataIO\DataIOReader;
+use Avro\DataIO\DataIOReaderSingleObjEnc;
+use Avro\DataIO\DataIOWriter;
+use Avro\DataIO\DataIOWriterSingleObjEnc;
+use Avro\Datum\IODatumReader;
+use Avro\Datum\IODatumWriter;
+use Avro\IO\StringIO;
+use Avro\Schema\Schema;
+use Avro\Registry\SchemaRegistry;
+
+class AvroSerDe
+{
+
+    public function serialize(string $sDataJson, Schema $oSchema, string $sPacketHeader = ''): string
+    {
+        $aData = json_decode($sDataJson, true) ?: [];
+
+        $oIO = new StringIO();
+        $oDateWriter = new DataIOWriterSingleObjEnc($oIO, new IODatumWriter($oSchema), $oSchema, $sPacketHeader);
+
+        foreach ($aData as $aItemDatum) {
+            $oDateWriter->append($aItemDatum);
+        }
+        $oDateWriter->close();
+
+        return $oIO->string();
+	}
+
+    public function deserialize(string $sPacket, SchemaRegistry $oSchemaRegistry): array
+    {
+        $oIO = new StringIO($sPacket);
+        $oDataReader = new DataIOReaderSingleObjEnc($oIO, new IODatumReader(), $oSchemaRegistry);
+
+        $aData = [];
+        foreach ($oDataReader->data() as $item) {
+            $aData[] = $item;
+        }
+
+        return $aData;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
