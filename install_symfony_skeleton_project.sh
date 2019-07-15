575ee6afe07bb71d9c459a85613b6ba0a06277f2
Mikl <mikl@mediagraphics.ru>
2019-07-15 16:47:53 +0300

    install_symfony_skeleton_project.sh webpack-encore bootstrap added

--- install_symfony_skeleton_project.sh
+++ install_symfony_skeleton_project.sh
@@ -6,6 +6,12 @@ composer create-project symfony/skeleton $(pwd)
 composer require symfony/twig-bundle
 composer require sensio/framework-extra-bundle
 composer require symfony/http-foundation
+composer require symfony/webpack-encore-bundle
+
+yarn install
+yarn add bootstrap --dev
+yarn add sass-loader@^7.0.1 node-sass --dev
+yarn add jquery popper.js
 
 composer config repositories.avro vcs https://github.com/northv/avro-php
 composer require apache/avro-php
@@ -14,5 +20,12 @@ echo "AVRO_REGISTRY_LINK=http://${1}/api/v1/schemaregistry" >> .env
 echo "AVRO_CONFLUENT_LINK=http://${1}/api/v1/confluent"     >> .env
 
 mkdir templates/AvroSerDe
-cp vendor/apache/avro-php/AvroBundle/Resources/views/AvroSerDe/index.html.twig       templates/AvroSerDe/index.html.twig
 cp vendor/apache/avro-php/AvroBundle/Controller/AppControllerAvroSerDeController.php src/Controller/AvroSerDeController.php
+cp vendor/apache/avro-php/AvroBundle/Resources/views/AvroSerDe/index.html.twig       templates/AvroSerDe/index.html.twig
+
+cat vendor/apache/avro-php/AvroBundle/Resources/views/base.html.twig               > templates/base.html.twig
+cat vendor/apache/avro-php/AvroBundle/Resources/assets/css/app.css                 > assets/css/app.css
+cat vendor/apache/avro-php/AvroBundle/Resources/assets/css/app.js                  > assets/js/app.js
+
+sed -i "s/\/\/.enableSassLoader()/.enableSassLoader()/" webpack.config.js
+yarn encore dev
